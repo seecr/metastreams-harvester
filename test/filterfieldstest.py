a393433b70c2f8a8c560f5c11efd071a8e4883a2
Thijs Janssen <thijs@seecr.nl>
2021-12-29 16:13:48 +0100

    TJ: updated license from Meresco to Metastreams.

--- test/filterfieldstest.py
+++ test/filterfieldstest.py
@@ -1,24 +1,25 @@
 ## begin license ##
 #
-# "Seecr Metastreams" is a fork of Meresco Harvester that demonstrates the translation of traditional metadata into modern events streams.
+# "Metastreams Harvester" is a fork of Meresco Harvester that demonstrates
+# the translation of traditional metadata into modern events streams.
 #
 # Copyright (C) 2019, 2021 Seecr (Seek You Too B.V.) https://seecr.nl
 # Copyright (C) 2019 Stichting Kennisnet https://www.kennisnet.nl
 #
-# This file is part of "Seecr Metastreams"
+# This file is part of "Metastreams Harvester"
 #
-# "Seecr Metastreams" is free software; you can redistribute it and/or modify
+# "Metastreams Harvester" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "Seecr Metastreams" is distributed in the hope that it will be useful,
+# "Metastreams Harvester" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Seecr Metastreams"; if not, write to the Free Software
+# along with "Metastreams Harvester"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
