2d0141145e967c44b8852adcff68e8a072db7600
Thijs Janssen <thijs@seecr.nl>
2021-12-29 16:13:22 +0100

    TJ: updated license.

--- usr-share/controlpanel/repositoryGroup.js
+++ usr-share/controlpanel/repositoryGroup.js
@@ -1,3 +1,28 @@
+/* begin license *
+ *
+ * "Metastreams Harvester" is a fork of Meresco Harvester that demonstrates
+ * the translation of traditional metadata into modern events streams.
+ *
+ * Copyright (C) 2021 Seecr (Seek You Too B.V.) https://seecr.nl
+ *
+ * This file is part of "Metastreams Harvester"
+ *
+ * "Metastreams Harvester" is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * "Metastreams Harvester" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Metastreams Harvester"; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * end license */
+
 function init_domainAttributes() {
     form_init(
         $("#FrmRepositoryGroupAttributes"), "/actions/updateRepositoryGroup",
