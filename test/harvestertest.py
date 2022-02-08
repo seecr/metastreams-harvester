59fec86a5161446bc9ebbe644fa3b8bc693ff98a
Thijs Janssen <thijs@seecr.nl>
2022-02-08 17:23:23 +0100

    TJ: Rename state.

--- test/harvestertest.py
+++ test/harvestertest.py
@@ -1,10 +1,7 @@
 ## begin license ##
 #
-# "Meresco Harvester" consists of two subsystems, namely an OAI-harvester and
-# a web-control panel.
-# "Meresco Harvester" is originally called "Sahara" and was developed for
-# SURFnet by:
-# Seek You Too B.V. (CQ2) http://www.cq2.nl
+# "Metastreams Harvester" is a fork of Meresco Harvester that demonstrates
+# the translation of traditional metadata into modern events streams.
 #
 # Copyright (C) 2006-2007 SURFnet B.V. http://www.surfnet.nl
 # Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
@@ -12,25 +9,25 @@
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
 # Copyright (C) 2009 Tilburg University http://www.uvt.nl
 # Copyright (C) 2010-2012, 2015, 2020-2021 Stichting Kennisnet https://www.kennisnet.nl
-# Copyright (C) 2012-2015, 2020-2021 Seecr (Seek You Too B.V.) https://seecr.nl
+# Copyright (C) 2012-2015, 2020-2022 Seecr (Seek You Too B.V.) https://seecr.nl
 # Copyright (C) 2020-2021 Data Archiving and Network Services https://dans.knaw.nl
 # Copyright (C) 2020-2021 SURF https://www.surf.nl
 # Copyright (C) 2020-2021 The Netherlands Institute for Sound and Vision https://beeldengeluid.nl
 #
-# This file is part of "Meresco Harvester"
+# This file is part of "Metastreams Harvester"
 #
-# "Meresco Harvester" is free software; you can redistribute it and/or modify
+# "Metastreams Harvester" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "Meresco Harvester" is distributed in the hope that it will be useful,
+# "Metastreams Harvester" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Meresco Harvester"; if not, write to the Free Software
+# along with "Metastreams Harvester"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
@@ -49,7 +46,7 @@ from seecr.test import CallTrace
 
 from meresco.components.json import JsonDict
 from meresco.harvester.harvester import Harvester
-from meresco.harvester.harvesterlog import HarvesterLog
+from meresco.harvester._harvesterlog import HarvesterLog
 from meresco.harvester.oairequest import OaiRequest
 from meresco.harvester.virtualuploader import InvalidDataException, TooMuchInvalidDataException
 from meresco.harvester.mapping import Mapping, DEFAULT_CODE, Upload
@@ -219,7 +216,7 @@ class HarvesterTest(unittest.TestCase):
             JsonDict({'resumptionToken': None, 'from': "1999-12-01T16:37:41Z"}).dump(fp)
 
         with open(self.stateDir + '/tud.ids', 'w') as f:
-            for i in range(113): 
+            for i in range(113):
                 f.write('oai:tudfakeid:%05i\n'%i)
         repository = self.MockRepository3('tud' ,'http://repository.tudelft.nl/oai', None, 'tud')
         logger = self.createLogger()
@@ -288,7 +285,7 @@ class HarvesterTest(unittest.TestCase):
         self.mockRepository = MockOaiRequest('mocktud')
         with open(self.stateDir + '/tud.stats', 'w') as f:
             f.write(' Started: 1999-12-01 16:37:41, Harvested/Uploaded/Total: 56/23/113, Done: 2004-12-31 16:39:15\n')
-        
+
         with open(self.stateDir + '/tud.next', 'w') as f:
             JsonDict({'resumptionToken': None, 'from': "2015-01-01T00:12:13Z"}).dump(f)
         repository = self.MockRepository3('tud' ,'http://repository.tudelft.nl/oai', None, 'tud', continuous=True)
